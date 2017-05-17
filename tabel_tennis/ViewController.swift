57fcf370af17dc96a9c841f49f7af3ca8ff9bec1
Lizn <wwwliuzhencom@vip.qq.com>
2017-05-17 12:03:23 +0800

    coredata

--- tabel_tennis/ViewController.swift
+++ tabel_tennis/ViewController.swift
@@ -175,6 +175,63 @@ class ViewController: UIViewController {
             score_a.text="\(temp)"
         }
         
+        if count_a+1>=10 || count_b+1>=10
+        {
+            if count_a+1==11 && count_b+1<=9{
+                count_a=0
+                count_b=0
+                result_a.text="Awin"
+                result_b.text="Blost"
+                point_a+=1
+                temp=0
+                temp_1=0
+                score_a.text="0"
+                score_b.text="0"
+                total_a.text="\(point_a)"
+                
+            }
+            else if count_b+1==11 && count_a+1<=9{
+                count_a=0
+                count_b=0
+                result_a.text="Alost"
+                result_b.text="Bwin"
+                point_b+=1
+                temp=0
+                temp_1=0
+                score_a.text="0"
+                score_b.text="0"
+                total_b.text="\(point_b)"
+            }
+        }
+        if count_a+1>=10 && count_b+1>=10
+        {
+            if count_a - count_b > 1{
+                count_a=0
+                count_b=0
+                result_a.text="Awin"
+                result_b.text="Blost"
+                point_a+=1
+                temp=0
+                temp_1=0
+                score_a.text="0"
+                score_b.text="0"
+                total_a.text="\(point_a)"
+            }
+            else if count_b - count_a > 1{
+                count_a=0
+                count_b=0
+                result_a.text="Alost"
+                result_b.text="Bwin"
+                point_b+=1
+                temp=0
+                temp_1=0
+                score_a.text="0"
+                score_b.text="0"
+                total_b.text="\(point_b)"
+            }
+        }
+        
+
     }
     @IBAction func undo_b(_ sender: Any) {
         temp_1 = temp_1 - 1
@@ -185,6 +242,64 @@ class ViewController: UIViewController {
         else {
             score_b.text="\(temp_1)"
         }
+        
+        if count_a+1>=10 || count_b+1>=10
+        {
+            if count_a+1==11 && count_b+1<=9{
+                count_a=0
+                count_b=0
+                result_a.text="Awin"
+                result_b.text="Blost"
+                point_a+=1
+                temp=0
+                temp_1=0
+                score_a.text="0"
+                score_b.text="0"
+                total_a.text="\(point_a)"
+                
+            }
+            else if count_b+1==11 && count_a+1<=9{
+                count_a=0
+                count_b=0
+                result_a.text="Alost"
+                result_b.text="Bwin"
+                point_b+=1
+                temp=0
+                temp_1=0
+                score_a.text="0"
+                score_b.text="0"
+                total_b.text="\(point_b)"
+            }
+        }
+        if count_a+1>=10 && count_b+1>=10
+        {
+            if count_a - count_b > 1{
+                count_a=0
+                count_b=0
+                result_a.text="Awin"
+                result_b.text="Blost"
+                point_a+=1
+                temp=0
+                temp_1=0
+                score_a.text="0"
+                score_b.text="0"
+                total_a.text="\(point_a)"
+            }
+            else if count_b - count_a > 1{
+                count_a=0
+                count_b=0
+                result_a.text="Alost"
+                result_b.text="Bwin"
+                point_b+=1
+                temp=0
+                temp_1=0
+                score_a.text="0"
+                score_b.text="0"
+                total_b.text="\(point_b)"
+            }
+        }
+        
+
 
     }
     @IBAction func clearall(_ sender: Any) {
